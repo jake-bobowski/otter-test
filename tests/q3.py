01d99e5b5bbab928c99cefcd7938fe8bf47a6536
Jake Bobowski <jake.bobowski@ubc.ca>
2022-09-06 20:26:44 -0700

    Add files via upload

---
+++ tests/q3.py
@@ -0,0 +1,2 @@
+OK_FORMAT = True
+test = {'name': 'q3', 'points': 1, 'suites': [{'cases': [{'code': '>>> a3 == 2**8 \nTrue', 'hidden': True, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
