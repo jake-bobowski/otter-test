22dd479cc122a4cf15ca59dd8c389ca07aab2e37
Jake Bobowski <jake.bobowski@ubc.ca>
2022-09-07 12:34:51 -0700

    Add files via upload

--- installer.py
+++ installer.py
@@ -18,7 +18,7 @@ def cstr(s, color = 'black'):
 ###############################################################################
 # Check to see if required packages are already installed.                    #
 # If not, then install them.                                                  #
-# - modified 20220906                                                         #
+# - modified 20220907                                                         #
 ############################################################################### 
 # Start the 'Check' function.
 def Check():
@@ -31,10 +31,24 @@ def Check():
         spec = importlib.util.find_spec(name)
         if spec is None:
             display(html_print(cstr('Installing some packages ...\n', color = 'red')))
+            display(html_print(cstr('After the installation completes, please completely log out and then login again before proceeding.\n', color = 'red')))
             subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', name])
             cnt += 1
-                
+
+    import importlib
+    try:
+        importlib.import_module('otter')
+    except ImportError:
+        display(html_print(cstr('Installing some packages ...\n', color = 'red')))
+        display(html_print(cstr('After the installation completes, please completely log out and then login again before proceeding.\n', color = 'red')))
+        import pip
+        pip.main(['install', 'otter-grader'])
+        cnt += 1
+    finally:
+        globals()['otter'] = importlib.import_module('otter')
+    
     if cnt == 0:
         display(html_print(cstr('All packages already installed. Please proceed.', color = 'black')))
     else:
         display(html_print(cstr('\n Some packages were installed.  Please completely log out and then login again before proceeding.', color = 'red')))
+        
